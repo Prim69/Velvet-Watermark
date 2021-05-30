173b9c1ef11d3722cb42e9853d51fe4b5fe3d7e2
Hashim <13991048+Prim69@users.noreply.github.com>
2021-05-30 00:48:05 -0400

    Add files via upload

---
+++ Velvet Overlay/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Velvet_Overlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Width = SystemParameters.PrimaryScreenWidth;
+            Left = 0;
+            Top = (SystemParameters.PrimaryScreenHeight / 2) - MainPanel.Height;
+            MainPanel.Width = SystemParameters.PrimaryScreenWidth;
+            MainText.Width = MainPanel.Width / 2;
+        }
+
+    }
+}
